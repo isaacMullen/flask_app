c4ffa82e7fedf4e94442a747363b4924a4fcea78
Isaac <isaacmullen030@gmail.com>
2025-04-03 20:45:18 -0300

    Added search functionality

--- app.py
+++ app.py
@@ -1,31 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
 
-DATABASE = "test.db"
+DATABASE = "nba.sqlite"
 
 def get_conn():
-    conn = sqlite3.connect(DATABASE)
+    conn = sqlite3.connect(DATABASE) # connecting to predefined database
     conn.row_factory = sqlite3.Row
     return conn
 
 @app.route("/")
-def index():
+def home():
+    return redirect(url_for('players'))
+
+@app.route('/players', methods=['GET'])
+def players():
     connection = get_conn()
     cursor = connection.cursor()
     
-    try:
-        cursor.execute('SELECT * FROM users')
-        users = cursor.fetchall()
-        print(f"Users fetched: {users}")  # Log the fetched data
-    except sqlite3.Error as e:
-        print(f"Database error: {e}")  # Catch any database errors and log them
-        users = []
-    finally:
-        connection.close()
+    search_query = request.args.get('player_name', '')
 
-    return render_template("index.html", items=users)
+    players = []
+    if search_query:
+        cursor.execute('SELECT * FROM player WHERE full_name LIKE ?', ('%' + search_query + '%',))
+        players = cursor.fetchall()
+
+    player = cursor.fetchall()
+    connection.close()
+
+    return render_template("players.html", players=players)
+
+@app.route('/teams', methods=['GET'])
+def teams():
+    connection = get_conn()
+    cursor = connection.cursor()
+    
+    search_query = request.args.get('team_name', '')
+
+    teams = []
+    if search_query:
+        cursor.execute('SELECT * FROM team WHERE full_name LIKE ?', ('%' + search_query + '%',))
+        teams = cursor.fetchall()
+
+    team = cursor.fetchall()
+    connection.close()
+
+    return render_template("teams.html", teams=teams)
 
 if __name__ == "__main__":
     app.run(debug=True)
