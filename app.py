b569c5a06d1636edff87f1ac9fa2b4b07a09b001
Mike Mullen <mikemullen@Mikes-MacBook-Pro.local>
2025-04-03 14:35:53 -0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+items = ["Isaac", "Mike", "Jack", "Adam"]
+
+@app.route("/")
+def index():
+    return render_template("index.html", items=items)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
