2f3c4af231fcc55be629fad6542b84c284bb30fd
Isaac <isaacmullen030@gmail.com>
2025-04-03 15:23:47 -0300

    Hooked up a test database

--- app.py
+++ app.py
@@ -3,12 +3,27 @@ import sqlite3
 
 app = Flask(__name__)
 
+DATABASE = "test.db"
+
+def get_conn():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
 items = ["Isaac", "Mike", "Jack", "Adam", "Joey"]
 
 @app.route("/")
 def index():
-    return render_template("index.html", items=items)
+    connection = get_conn()
+    cursor = connection.cursor()
 
+    cursor.execute('SELECT * FROM users')
+    users = cursor.fetchall()
+    
+
+    connection.close()
+
+    return render_template("index.html", items=users)
 
 if __name__ == "__main__":
     app.run(debug=True)
