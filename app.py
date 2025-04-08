96eb299b419d006a71d05683035c75729725b71f
Isaac <isaacmullen030@gmail.com>
2025-04-08 16:48:31 -0300

    Created virtual tables, added Full-Text-Search capability

--- app.py
+++ app.py
@@ -26,42 +26,19 @@ def players():
     players = [] # will store the players returned by the query
     if search_query:
         # if the user provided a search parameter, execute an SQL query to retrieve the players that match the query
-        cursor.execute('SELECT * FROM player WHERE full_name LIKE ?', ('%' + search_query + '%',))
+        cursor.execute('SELECT * FROM common_player_info_fts WHERE full_name MATCH ? OR last_name MATCH ?', (search_query, search_query))
         players = cursor.fetchall() # storing the results inside players[]
+
+        if not players:
+            cursor.execute('SELECT * FROM common_player_info_fts WHERE full_name LIKE ?', ('%' + search_query + '%',))
+            players = cursor.fetchall() # storing the results inside players[]
+
 
     connection.close()
 
     # display the data retrieved by the query
     return render_template("players.html", players=players)
 
-# team search route
-@app.route('/teams', methods=['GET'])
-def teams():
-    connection = get_conn() # establishing connection
-    cursor = connection.cursor() # cursor to execute queries
-    
-    search_query = request.args.get('team_name', '') # gets the first query parameter if one exists
-    search_query_two = request.args.get('team_abbreviation', '') # gets the second query parameter if one exists
-
-    teams = [] # returned data will be stored in a list of objects
-    if search_query:
-        # if there is at least one query parameter present, search the database using the search query provided by the user
-        query = '''select team.id, team.full_name, team_details.city, team_details.abbreviation, team_details.team_id
-                from team 
-                join team_details on team.id = team_details.team_id where team.full_name LIKE ?'''
-        params = ['%' + search_query + '%'] # adding the returned data to a list       
-        
-        if search_query_two:
-            # if there are two search qeuries, modifying the SQL query and add another parameter to search on
-            query += '''and team_details.abbreviation like ?'''
-            params.append('%' + search_query_two + '%')
-    
-        cursor.execute(query, params) # exceuting the SQL squery that was built
-        teams = cursor.fetchall() # storing all returned data in teams[]
-    
-    connection.close()
-
-    return render_template("teams.html", teams=teams) 
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5001, debug=True)
